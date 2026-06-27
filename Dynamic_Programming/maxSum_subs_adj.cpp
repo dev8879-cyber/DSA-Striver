3fbe976f780c9bb57da79d8a1483352e05bae3f8
Dev Barot <devbarot8879@gmail.com>
2026-06-27 12:59:55 +0530

    Dp starters

--- Dynamic_Programming/maxSum_subs_adj.cpp
+++ Dynamic_Programming/maxSum_subs_adj.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Recursion:
@@ -62,8 +62,49 @@ using namespace std;
 
 // Tabulation::
 
-int Sum_sub(int n,vector<int>arr){
-    vector<int>dp(n);
+// int Sum_sub(int n, vector<int> arr)
+// {
+//     vector<int> dp(n);
+//     dp[0] = arr[0];
+//     for (int i = 1; i < n; i++)
+//     {
+//         int pick = arr[i];
+//         if (n > 1)
+//         {
+//             pick += dp[i - 2];
+//         }
+//         int no_pick = dp[i - 1];
+//         dp[n] = max(pick, no_pick);
+//     }
+//     return dp[n - 1];
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int elem : arr)
+//     {
+//         cin >> elem;
+//     }
+//     cout << Sum_sub(n - 1, arr);
+// }
+
+// Space Optimization:
+int Sum_sub(int n,vector<int>& arr){
+    int prev=arr[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int pick=arr[i];
+        if(i>1){
+            pick+=prev2;
+        }
+        int not_pick=prev;
+        int curi=max(pick,not_pick);
+        prev2=prev;
+        prev=curi;
+    }
+    return prev;
 }
 int main(){
     int n;
