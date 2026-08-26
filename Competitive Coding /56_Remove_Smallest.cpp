ccffe478e7338204406276dbbf3f9c95e9b57841
Dev Barot <devbarot8879@gmail.com>
2026-08-27 04:42:24 +0530

    Level-2

---
+++ Competitive Coding /56_Remove_Smallest.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+
+        bool possible = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i + 1] - a[i] > 1) {
+                possible = false;
+                break;
+            }
+        }
+
+        if (possible)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
